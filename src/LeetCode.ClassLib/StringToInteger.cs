102184e542be82ed530b0d1ecaa1ac3e0101228a
OpticGenius <opticgenius@hotmail.com>
2018-09-14 00:13:07 +1000

    Added question descriptions

--- src/LeetCode.ClassLib/StringToInteger.cs
+++ src/LeetCode.ClassLib/StringToInteger.cs
@@ -1,6 +1,27 @@
 using System;
 using System.Text;
 
+/*
+
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. 
+Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+
+Only the space character ' ' is considered as whitespace character.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+
+ */
+
 namespace LeetCode.ClassLib {
     public class StringToInteger {
 
@@ -30,12 +51,10 @@ namespace LeetCode.ClassLib {
                         stringBuffer.Append (character);
                     }
 
-                    // Attempt to return a valid integer
                     try {
                         int number = Int32.Parse (stringBuffer.ToString ());
                         return number;
 
-                        // Handle exceptions
                     } catch (Exception ex) {
                         if (ex is OverflowException) {
 
